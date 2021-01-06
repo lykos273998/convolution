90027eb431c7d12bfda18ca3de94a35b17b42b9b
lykos273998 <francy273998@gmail.com>
2021-01-06 02:10:36 +0100

    dd

--- mpi_qq.c
+++ mpi_qq.c
@@ -1465,12 +1465,7 @@ void exchange_halos_2B(unsigned short* my_img,int* my_img_dims, unsigned short**
         MPI_Recv(halo_down, s*ncols, MPI_UNSIGNED_SHORT, rd, tag, mpi_communicator, &status);  
         }
 
-    if(send = 1){
-        send = 0;
-        MPI_Wait(&request, &status);
-    }
-    
-    
+   
 
     if(rd >= 0){
         send = 1;
@@ -1496,11 +1491,6 @@ void exchange_halos_2B(unsigned short* my_img,int* my_img_dims, unsigned short**
         MPI_Recv(halo_up, s*ncols, MPI_UNSIGNED_SHORT, ru, my_rank, mpi_communicator, &status);  
         }
 
-    if(send = 1){
-        send = 0;
-        MPI_Wait(&request, &status);
-    }
-    
     
 
     if(rr >= 0 ){
@@ -1526,10 +1516,7 @@ void exchange_halos_2B(unsigned short* my_img,int* my_img_dims, unsigned short**
         MPI_Recv(halo_left, s*nrows, MPI_UNSIGNED_SHORT, rl, my_rank, mpi_communicator, &status)  ;
         }
 
-    if(send = 1){
-        send = 0;
-        MPI_Wait(&request, &status);
-    }
+    
     
     if(rl >= 0 ){
         send = 1;
