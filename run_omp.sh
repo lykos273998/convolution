35673938a90d4df4867b79cfac2766e86fec59a6
lykos273998 <francy273998@gmail.com>
2021-01-06 14:38:04 +0100

    time meas

---
+++ run_omp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd roba_varia/convolution
+module load openmpi-4.0.5+gnu-9.3.0
+
+in_file="images/test_picture.pgm"
+out_file="out.pgm"
+
+./compile.sh
+
+kernel_size="101"
+kernel_type="1"
+w="0.2"
+timing_path="timing/omp/"
+for run in {1..3}
+do
+
+
+
+    for procs in {1..48}; 
+    do
+    time_file=$timing_path$run_"t".$procs
+    export $OMP_NUM_THREADS=$procs
+    echo "executing on ", ${procs}, "  processors"
+    /usr/bin/time ./blur_omp $kernel_type $kernel_size $in_file $out_file 2> $timing_path
+    done
+
+done
