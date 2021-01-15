36d10a9e89ea19f6068068c0dd06ac3c06aa6b64
lykos273998 <francy273998@gmail.com>
2021-01-16 00:17:12 +0100

    rr

--- weak.sh
+++ weak.sh
@@ -12,125 +12,125 @@ gcc read_write_pgm_image.c -o rw
 kernel_size=101
 kernel_type="1"
 w="0.2"
-mpi_path="timing/mpi/weak"
-omp_path="timing/mpi/weak"
-
-./rw 65535 1000 1000
-procs=1
-
-time_file=$omp_path_"w".$procs
-export OMP_NUM_THREADS=$procs
-/usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
-
-time_file=$mpi_path_"w".$procs
-/usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
-
-./rw 65535 1000 2000
-procs=2
-
-time_file=$omp_path_"w".$procs
-export OMP_NUM_THREADS=$procs
-/usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
-
-time_file=$mpi_path_"w".$procs
-/usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
+mpi_path_="timing/mpi/weak"
+omp_path_="timing/omp/weak"
 
 ./rw 65535 2000 2000
-procs=4
+procs=1
 
-time_file=$omp_path_"w".$procs
+time_file=${omp_path_}"w".$procs
 export OMP_NUM_THREADS=$procs
 /usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
 
-time_file=$mpi_path_"w".$procs
+time_file=${mpi_path_}"w".$procs
 /usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
 
 ./rw 65535 2000 4000
-procs=8
+procs=2
 
-time_file=$omp_path_"w".$procs
+time_file=${omp_path_}"w".$procs
 export OMP_NUM_THREADS=$procs
 /usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
 
-time_file=$mpi_path_"w".$procs
-/usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
-
-./rw 65535 2000 5000
-procs=10
-
-time_file=$omp_path_"w".$procs
-export OMP_NUM_THREADS=$procs
-/usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
-
-time_file=$mpi_path_"w".$procs
-/usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
-
-./rw 65535 4000 3000
-procs=12
-
-time_file=$omp_path_"w".$procs
-export OMP_NUM_THREADS=$procs
-/usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
-
-time_file=$mpi_path_"w".$procs
-/usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
-
-./rw 65535 7000 2000
-procs=14
-
-time_file=$omp_path_"w".$procs
-export OMP_NUM_THREADS=$procs
-/usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
-
-time_file=$mpi_path_"w".$procs
+time_file=${mpi_path_}"w".$procs
 /usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
 
 ./rw 65535 4000 4000
+procs=4
+
+time_file=${omp_path_}"w".$procs
+export OMP_NUM_THREADS=$procs
+/usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
+
+time_file=${mpi_path_}"w".$procs
+/usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
+
+./rw 65535 4000 8000
+procs=8
+
+time_file=${omp_path_}"w".$procs
+export OMP_NUM_THREADS=$procs
+/usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
+
+time_file=${mpi_path_}"w".$procs
+/usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
+
+./rw 65535 4000 10000
+procs=10
+
+time_file=${omp_path_}"w".$procs
+export OMP_NUM_THREADS=$procs
+/usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
+
+time_file=${mpi_path_}"w".$procs
+/usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
+
+./rw 65535 8000 6000
+procs=12
+
+time_file=${omp_path_}"w".$procs
+export OMP_NUM_THREADS=$procs
+/usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
+
+time_file=${mpi_path_}"w".$procs
+/usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
+
+./rw 65535 14000 4000
+procs=14
+
+time_file=${omp_path_}"w".$procs
+export OMP_NUM_THREADS=$procs
+/usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
+
+time_file=${mpi_path_}"w".$procs
+/usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
+
+./rw 65535 8000 8000
 procs=16
 
-time_file=$omp_path_"w".$procs
+time_file=${omp_path_}"w".$procs
 export OMP_NUM_THREADS=$procs
 /usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
 
-time_file=$mpi_path_"w".$procs
+time_file=${mpi_path_}"w".$procs
 /usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
 
-./rw 65535 9000 2000
+./rw 65535 18000 4000
 procs=18
 
-time_file=$omp_path_"w".$procs
+time_file=${omp_path_}"w".$procs
 export OMP_NUM_THREADS=$procs
 /usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
 
-time_file=$mpi_path_"w".$procs
+time_file=${mpi_path_}"w".$procs
 /usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
 
-./rw 65535 10000 2000
+./rw 65535 20000 4000
 procs=20
 
-time_file=$omp_path_"w".$procs
+time_file=${omp_path_}"w".$procs
 export OMP_NUM_THREADS=$procs
 /usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
 
-time_file=$mpi_path_"w".$procs
+time_file=${mpi_path_}"w".$procs
 /usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
 
-./rw 65535 11000 2000
+./rw 65535 22000 4000
 procs=22
 
-time_file=$omp_path_"w".$procs
+time_file=${omp_path_}"w".$procs
 export OMP_NUM_THREADS=$procs
 /usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
 
-time_file=$mpi_path_"w".$procs
+time_file=${mpi_path_}"w".$procs
 /usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
 
-./rw 65535 6000 4000
+./rw 65535 12000 8000
 procs=24
 
-time_file=$omp_path_"w".$procs
+time_file=${omp_path_}"w".$procs
 export OMP_NUM_THREADS=$procs
 /usr/bin/time ./blur_omp $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
 
-time_file=$mpi_path_"w".$procs
+time_file=${mpi_path_}"w".$procs
 /usr/bin/time mpirun -np $procs --mca btl '^openib' blur_mpi $kernel_type $kernel_size $w $in_file $out_file 2> $time_file
